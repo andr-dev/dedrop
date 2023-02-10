4207827c220a57fa5c48f4a921bf31635d20663c
a <a@gmail.com>
2023-02-10 17:08:45 -0500

    Add new App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import StreamrClient, { StreamPermission } from 'streamr-client';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import StreamrClient, { StreamPermission } from "streamr-client";
 
-import { invoke } from '@tauri-apps/api'
+import { invoke } from "@tauri-apps/api";
 
 function App() {
   return (
@@ -16,23 +16,24 @@ function App() {
 async function streamrTest() {
   const streamrClient = new StreamrClient({
     auth: {
-      privateKey: '',
+      privateKey: "",
     },
   });
 
   const stream = await streamrClient.getOrCreateStream({
-    id: '/foo/bar',
+    id: "/foo/bar",
   });
 
   const streamId = stream.id;
 
   console.log(`connected to streamId ${streamId}`, stream);
 
-  console.log("finished granting perms");
-
-
   setInterval(async () => {
-    console.log("sent!", await stream.publish({ test: "yeah" }));
+    invoke("load_file", {
+      path: "/Users/alber/Desktop/penn/airdrop/public/robots.txt",
+    })
+      .then((res) => stream.publish(res))
+      .catch(console.log);
   }, 1000);
 }
 
