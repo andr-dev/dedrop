e0f8edab94cb7e25407a9bd6fd15d6740443e243
andrthesnek <andr@the.snek>
2023-02-10 16:37:46 -0500

    add streamr publish

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StreamrClient, { StreamPermission } from 'streamr-client';
 
 import { invoke } from '@tauri-apps/api'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={streamrTest}>streamer</button>
     </div>
   );
+}
+
+async function streamrTest() {
+  const streamrClient = new StreamrClient({
+    auth: {
+      privateKey: '',
+    },
+  });
+
+  const stream = await streamrClient.getOrCreateStream({
+    id: '/foo/bar',
+  });
+
+  const streamId = stream.id;
+
+  console.log(`connected to streamId ${streamId}`, stream);
+
+  console.log("finished granting perms");
+
+
+  setInterval(async () => {
+    console.log("sent!", await stream.publish({ test: "yeah" }));
+  }, 1000);
 }
 
 export default App;
