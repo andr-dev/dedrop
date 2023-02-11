2503a78d01d64fefef0f085247937c2550f3d684
user <dev@johnatangao.com>
2023-02-11 16:28:45 -0500

    Add button contacts

--- src/screens/Contacts.tsx
+++ src/screens/Contacts.tsx
@@ -1,30 +1,64 @@
 import { Box, Container, Grid, List, Stack, TextField } from "@mui/material";
 import { UserCard } from "./receive/UserCard";
-import { ContactCard } from "./contacts/ContactCard";
 import { FixedSizeList, ListChildComponentProps } from 'react-window';
+import { invoke } from "@tauri-apps/api";
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import { AddCircle, UndoRounded } from "@mui/icons-material";
+import ContactCard from "./contacts/ContactCard";
 
 export type Contacts = Map<string, string>;
 
 export default function ContactsScreen() {
+  let [contacts, setContacts] = useState<string[][]>([]);
+  let [currentContactIdx, setCurrentContactIdx] = useState<number>();
+
+  useEffect(() => {
+    invoke("filter_contacts", { "filter": "" }).then((val) => {
+      setContacts(Object.entries(val as Contacts));
+    });
+  }, []);
+
   return (
-    // <Grid container columnSpacing={2}>
     <Stack direction="row" columnGap={2}>
       <Stack spacing={2}>
-        <TextField
-          fullWidth
-          id="address-or-contact"
-          label="Search for an address or contact"
-          variant="outlined"
-          size="small"
-          style={{ background: "#222B3A", borderWidth: "0px" }}
-        ></TextField>
+        <Stack direction="row">
+          <TextField
+            fullWidth
+            id="address-or-contact"
+            label="Search for an address or contact"
+            variant="outlined"
+            size="small"
+            style={{ background: "#222B3A", borderWidth: "0px" }}
+          ></TextField>
+          <Button onClick={() => {
+            setCurrentContactIdx(undefined)
+            console.log(currentContactIdx)
+          }}>
+            <AddCircle></AddCircle>
+          </Button>
+        </Stack>
+
         <List style={{ maxHeight: '100%', overflow: 'auto' }}>
-          <UserCard name={"Matt"} />
-          <UserCard name={"Andre"} />
+          {contacts.map(([name, publicKey], value) => <Button key={publicKey} onClick={() => {
+            setCurrentContactIdx(value)
+            console.log(currentContactIdx)
+          }}>
+            <UserCard name={name} />
+          </Button>)}
         </List>
       </Stack>
 
-      <ContactCard></ContactCard>
+      {currentContactIdx !== undefined && 
+        <ContactCard 
+          name={contacts[currentContactIdx][0]} 
+          address={contacts[currentContactIdx][1]}
+        />}
+
+      {currentContactIdx === undefined && <ContactCard 
+          name=""
+          address=""
+        />}
 
     </Stack>
   );
--- src/screens/contacts/ContactCard.tsx
+++ src/screens/contacts/ContactCard.tsx
@@ -1,26 +1,42 @@
-import { Box, Card, CardContent, CardHeader, Grid, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CardHeader, Grid, Stack, TextField, Typography } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import "@fontsource/rubik";
 import PlaceholderPic from "../../assets/contacts-profile-placeholder.png";
+import { Save } from "@mui/icons-material";
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api";
 
-export const ContactCard = () => (
-  <Card sx={{ background: "#222B3A", borderRadius: "20px" }}>
+interface Contact {
+  name: string
+  address: string
+}
+
+export default function ContactCard({name, address}: {name: string, address: string}) {
+
+  let [contact, setContact] = useState<Contact>({
+    name: name,
+    address: address
+  });
+
+  return (<Card sx={{ background: "#222B3A", borderRadius: "20px" }}>
     <CardContent>
       <Stack pl={2} pr={2} spacing={2}>
-        <Typography
+        <Stack direction="row" space-around>
+          <Typography
           sx={{ fontSize: 30, fontFamily: "Rubik" }}
           color="text.secondary"
         >
           Contacts
         </Typography>
-        <img
-          src={PlaceholderPic}
-          alt="Placeholder Pic"
-          style={{
-            borderRadius: '20px'
-          }}
-        />
+
+        <Button onClick={() => {
+          invoke("add_contact", { "name": contact.name, "stream": contact.address });
+        }}>
+          <Save></Save>
+        </Button>
+        </Stack>
+        
         <Stack direction="row" spacing={1} display="flex" alignItems="center">
           <AccountCircleIcon
             fontSize="large"
@@ -32,6 +48,11 @@ export const ContactCard = () => (
             variant="outlined"
             size="small"
             style={{ background: "#222B3A", borderWidth: "0px" }}
+            defaultValue={name}
+            onChange={(e) => setContact({
+              name: e.target.value,
+              address: contact.address
+            })}
           ></TextField>
         </Stack>
         <Stack direction="row" spacing={1} display="flex" alignItems="center">
@@ -45,9 +66,15 @@ export const ContactCard = () => (
             variant="outlined"
             size="small"
             style={{ background: "#222B3A", borderWidth: "0px" }}
+            defaultValue={address}
+            onChange={(e) => setContact({
+              name: contact.name,
+              address: e.target.value
+            })}
           ></TextField>
         </Stack>
       </Stack>
     </CardContent>
   </Card>
-);
+)
+}
