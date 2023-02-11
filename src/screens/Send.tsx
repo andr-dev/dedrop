417701357af5fbdb628b625eff5a18c5ab22fa9e
a <a@gmail.com>
2023-02-10 19:33:37 -0500

    Add send screen

--- src/screens/Send.tsx
+++ src/screens/Send.tsx
@@ -1,3 +1,56 @@
+import React from "react";
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import SendIcon from "@mui/icons-material/Send";
+import SystemUpdateAltIcon from "@mui/icons-material/SystemUpdateAlt";
+
 export default function SendScreen() {
-    return <div>send</div>
+  return (
+    <Box sx={{ auto: 250 }} role="presentation">
+      <List>
+        <br></br>
+        <ListItem key={"home"} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SendIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Send"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={"send"} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SendIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Send"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={"receive"} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SystemUpdateAltIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Receive"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={"contacts"} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ContactsIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Contacts"} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
 }
