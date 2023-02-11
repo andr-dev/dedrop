27edc9f0891ce2887efc6cd24940e00dda4acb29
a <a@gmail.com>
2023-02-11 01:50:50 -0500

    Add Share settings

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,22 +1,24 @@
-import { Navigate, Route, RouteProps } from 'react-router-dom';
-import Overview from './Overview';
-import Receive from './Receive';
-import Send from './Send';
+import { Navigate, Route, RouteProps } from "react-router-dom";
+import Overview from "./Overview";
+import Receive from "./Receive";
+import Send from "./Send";
+import ShareSettings from "./ShareSettings";
 
 const routes: RouteProps[] = [
-    {
-        index: true,
-        element: <Navigate to="overview" relative="route" />
-    },
-    { path: 'overview', element: Overview() },
-    { path: 'send', element: Send() },
-    { path: 'receive', element: Receive() },
+  {
+    index: true,
+    element: <Navigate to="overview" relative="route" />,
+  },
+  { path: "overview", element: Overview() },
+  { path: "send", element: Send() },
+  { path: "receive", element: Receive() },
+  { path: "share-settings", element: ShareSettings() },
 ];
 
 export default (
-    <>
-        {routes.map((route) => (
-            <Route key={route.path ?? ""} {...route} />
-        ))}
-    </>
+  <>
+    {routes.map((route) => (
+      <Route key={route.path ?? ""} {...route} />
+    ))}
+  </>
 );
