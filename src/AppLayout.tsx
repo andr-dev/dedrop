f70bb41ecbbbc3f15c90b725bef174fa0554b405
andrthesnek <andr@the.snek>
2023-02-10 20:50:27 -0500

    move sidebar to layout

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,6 +1,53 @@
-import React from "react";
+import { Box, ButtonBase, Stack, Typography } from "@mui/material";
+import React, { ReactNode } from "react";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import SendIcon from "@mui/icons-material/Send";
+import SystemUpdateAltIcon from "@mui/icons-material/SystemUpdateAlt";
+
+import Logo from "./assets/logo.png";
+
+
+interface SideBarProps {
+    label: string,
+    icon: ReactNode,
+}
+
+function SideBarItem(props: SideBarProps) {
+    return (
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', width: '100%' }} >
+            <ButtonBase sx={{ width: "100%", height: 96 }}>
+                <Stack justifyContent="center" alignItems="center" gap={1}>
+                    {props.icon}
+                    <Typography variant="subtitle2">{props.label}</Typography>
+                </Stack>
+            </ButtonBase>
+        </Box >
+    );
+}
+
+function SideBar() {
+    return (
+        <Box p={1} height="100%">
+            <Box
+                height="100%"
+                sx={{
+                    backgroundColor: "#222B3A",
+                    borderRadius: "20px",
+                }}
+                role="presentation"
+            >
+                <Stack>
+                    <img style={{ margin: 12 }} src={Logo} />
+                    <SideBarItem label="Send" icon={<SendIcon />} />
+                    <SideBarItem label="Receive" icon={<SystemUpdateAltIcon />} />
+                    <SideBarItem label="Contacts" icon={<ContactsIcon />} />
+                </Stack>
+            </Box>
+        </Box>
+    );
+}
 
 export function AppLayout() {
     // const loader = <Loader />;
@@ -8,8 +55,22 @@ export function AppLayout() {
     return (
         // <React.Suspense fallback={loader}>
         <Suspense fallback={<div className="w-screen h-screen bg-app" />}>
-            <Outlet />
-        </Suspense>
+            <div style={{ display: "flex", flexDirection: "row", width: "100vw", height: "100vh" }}>
+                <div style={{ width: 96, height: "100%" }}>
+                    <SideBar />
+                </div>
+                <div style={{ flexGrow: 1 }}>
+                    <Outlet />
+                </div>
+            </div>
+            {/* <Grid item xs={2}>
+                <SideBar></SideBar>
+            </Grid>
+            <Grid item xs>
+                <Outlet />
+            </Grid>
+        </Grid> */}
+        </Suspense >
         // </React.Suspense>
     );
 }
--- src/screens/Send.tsx
+++ src/screens/Send.tsx
@@ -3,93 +3,35 @@ import {
   Box,
   Button,
   Container,
-  Grid,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemButton,
-  ListItemText,
 } from "@mui/material";
-import ContactsIcon from "@mui/icons-material/Contacts";
-import SendIcon from "@mui/icons-material/Send";
-import SystemUpdateAltIcon from "@mui/icons-material/SystemUpdateAlt";
-import Logo from "../assets/logo.png";
 import "@fontsource/rubik";
-import { FileDrop } from "./send/FileDrop";
 
 export default function SendScreen() {
-  const SideBar = () => (
+  const FileDrop = () => (
     <Box
       sx={{
-        width: "40%",
-        height: "60%",
+        width: "1116px",
+        height: "765px",
         backgroundColor: "#222B3A",
-        borderRadius: "25px",
+        borderRadius: "20px",
       }}
       role="presentation"
     >
-      <List style={{ textAlign: "center" }}>
-        <ListItem
-          key={"send"}
-          disablePadding
-          style={{ display: "inline-block", textAlign: "center" }}
+      <Container>
+        <Box
+          sx={{
+            height: "670px",
+            width: "720px",
+            border: "dashed",
+            borderRadius: "20px",
+          }}
         >
-          <div>
-            <ListItemButton style={{ display: "block", textAlign: "center" }}>
-              <ListItemIcon style={{ display: "block" }}>
-                <img src={Logo} style={{ width: "50%" }}></img>
-              </ListItemIcon>
-            </ListItemButton>
-          </div>
-        </ListItem>
-        <br></br>
-        <br></br>
-        <br></br>
-        <ListItem
-          key={"send"}
-          disablePadding
-          style={{ display: "inline-block", textAlign: "center" }}
-        >
-          <div>
-            <ListItemButton style={{ display: "block", textAlign: "center" }}>
-              <ListItemIcon style={{ display: "block" }}>
-                <SendIcon />
-              </ListItemIcon>
-              <ListItemText primary={"Send"} style={{ display: "block" }} />
-            </ListItemButton>
-          </div>
-        </ListItem>
-        <ListItem
-          key={"receive"}
-          disablePadding
-          style={{ display: "inline-block", textAlign: "center" }}
-        >
-          <div>
-            <ListItemButton style={{ display: "block", textAlign: "center" }}>
-              <ListItemIcon style={{ display: "block" }}>
-                <SystemUpdateAltIcon />
-              </ListItemIcon>
-              <ListItemText primary={"Receive"} style={{ display: "block" }} />
-            </ListItemButton>
-          </div>
-        </ListItem>
-        <ListItem
-          key={"contacts"}
-          disablePadding
-          style={{ display: "inline-block", textAlign: "center" }}
-        >
-          <div>
-            <ListItemButton style={{ display: "block", textAlign: "center" }}>
-              <ListItemIcon style={{ display: "block" }}>
-                <ContactsIcon />
-              </ListItemIcon>
-              <ListItemText primary={"Contacts"} style={{ display: "block" }} />
-            </ListItemButton>
-          </div>
-        </ListItem>
-      </List>
+          <Button variant="contained">Select files to Send</Button>
+        </Box>
+      </Container>
     </Box>
   );
-
-  return <FileDrop></FileDrop>;
+  return (
+    <FileDrop />
+  );
 }
